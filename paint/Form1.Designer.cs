94d2bf5c5363e53f4835839afd5df004c7b6444f
Marcin <marcin.juranek2@gmail.com>
2019-05-27 06:29:49 +0200

    firts commit

---
+++ paint/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvasPanel = new System.Windows.Forms.Panel();
+            this.gBox = new System.Windows.Forms.GroupBox();
+            this.nudTRackBar = new System.Windows.Forms.NumericUpDown();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.grbTools = new System.Windows.Forms.GroupBox();
+            this.nudAngle = new System.Windows.Forms.NumericUpDown();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNew = new System.Windows.Forms.ToolStripButton();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.gBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTRackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.grbTools.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAngle)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // canvasPanel
+            // 
+            this.canvasPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.canvasPanel.BackColor = System.Drawing.Color.White;
+            this.canvasPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvasPanel.Location = new System.Drawing.Point(12, 27);
+            this.canvasPanel.Name = "canvasPanel";
+            this.canvasPanel.Size = new System.Drawing.Size(755, 460);
+            this.canvasPanel.TabIndex = 0;
+            this.canvasPanel.SizeChanged += new System.EventHandler(this.canvasPanel_SizeChanged);
+            this.canvasPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseDown);
+            this.canvasPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseMove);
+            this.canvasPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvasPanel_MouseUp);
+            this.canvasPanel.Resize += new System.EventHandler(this.canvasPanel_Resize);
+            // 
+            // gBox
+            // 
+            this.gBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gBox.Controls.Add(this.nudTRackBar);
+            this.gBox.Controls.Add(this.trackBar1);
+            this.gBox.Controls.Add(this.colorPanel);
+            this.gBox.Location = new System.Drawing.Point(12, 493);
+            this.gBox.Name = "gBox";
+            this.gBox.Size = new System.Drawing.Size(851, 98);
+            this.gBox.TabIndex = 1;
+            this.gBox.TabStop = false;
+            this.gBox.Text = "Kolor i grubość";
+            // 
+            // nudTRackBar
+            // 
+            this.nudTRackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudTRackBar.Location = new System.Drawing.Point(767, 38);
+            this.nudTRackBar.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudTRackBar.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudTRackBar.Name = "nudTRackBar";
+            this.nudTRackBar.Size = new System.Drawing.Size(60, 26);
+            this.nudTRackBar.TabIndex = 3;
+            this.nudTRackBar.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudTRackBar.ValueChanged += new System.EventHandler(this.nudTRackBar_ValueChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(76, 25);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Minimum = 2;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(698, 64);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.BackColor = System.Drawing.Color.Black;
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPanel.Location = new System.Drawing.Point(6, 25);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(64, 64);
+            this.colorPanel.TabIndex = 0;
+            this.colorPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.colorPanel_Paint);
+            this.colorPanel.DoubleClick += new System.EventHandler(this.colorPanel_DoubleClick);
+            // 
+            // grbTools
+            // 
+            this.grbTools.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grbTools.Controls.Add(this.nudAngle);
+            this.grbTools.Controls.Add(this.button9);
+            this.grbTools.Controls.Add(this.button8);
+            this.grbTools.Controls.Add(this.button7);
+            this.grbTools.Controls.Add(this.button6);
+            this.grbTools.Controls.Add(this.button5);
+            this.grbTools.Controls.Add(this.button4);
+            this.grbTools.Controls.Add(this.button3);
+            this.grbTools.Controls.Add(this.button2);
+            this.grbTools.Controls.Add(this.button1);
+            this.grbTools.Location = new System.Drawing.Point(773, 12);
+            this.grbTools.Name = "grbTools";
+            this.grbTools.Size = new System.Drawing.Size(103, 475);
+            this.grbTools.TabIndex = 2;
+            this.grbTools.TabStop = false;
+            this.grbTools.Text = "Narzędzia";
+            // 
+            // nudAngle
+            // 
+            this.nudAngle.Location = new System.Drawing.Point(7, 446);
+            this.nudAngle.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.nudAngle.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.nudAngle.Name = "nudAngle";
+            this.nudAngle.Size = new System.Drawing.Size(75, 26);
+            this.nudAngle.TabIndex = 12;
+            this.nudAngle.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(7, 407);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 32);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "cofnij";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(7, 372);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 28);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "obrót";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 314);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 51);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "rpostokat pelny";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 250);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 57);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "gumka";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 188);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 56);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "elipsa pełna";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Elipsa";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Pęddzel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Linia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Prostkąt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNew,
+            this.btnOpen,
+            this.btnSave});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(875, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNew
+            // 
+            this.btnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNew.Image = ((System.Drawing.Image)(resources.GetObject("btnNew.Image")));
+            this.btnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(23, 22);
+            this.btnNew.Text = "toolStripButton1";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(23, 22);
+            this.btnOpen.Text = "toolStripButton2";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(23, 22);
+            this.btnSave.Text = "toolStripButton3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 603);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.grbTools);
+            this.Controls.Add(this.gBox);
+            this.Controls.Add(this.canvasPanel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Paint";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gBox.ResumeLayout(false);
+            this.gBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTRackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.grbTools.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAngle)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvasPanel;
+        private System.Windows.Forms.GroupBox gBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.GroupBox grbTools;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown nudTRackBar;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNew;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.NumericUpDown nudAngle;
+    }
+}
+
---
+++ paint/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace paint
+{
+    public partial class Form1 : Form
+    {
+        private SolidBrush myBrush;
+        private Graphics myGraphics;
+        private Pen myPen;
+        private Point startXY = new Point(0, 0);
+        private bool elipseUse = false;
+        private bool penUse = false;
+        private bool brushUse = false;
+        private bool isDrawning = false;
+        private bool rectangleUse = false;
+        private bool filElipseUse = false;
+        private bool filRectangle = false;
+        private bool eraseUse = false;
+        Bitmap obrazek2;
+        Bitmap old;
+        private float angle = 0.0f;
+        private Bitmap buffer;
+        int heightR = 0, widthR = 0;
+        Point pStart, pEnd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initializeTools();
+        }
+        void initializeTools()
+        {
+            float widthBrush = int.Parse(nudTRackBar.Text);
+            myBrush = new SolidBrush(colorPanel.BackColor);
+            myGraphics = canvasPanel.CreateGraphics();
+            myPen = new Pen(colorPanel.BackColor,widthBrush);
+            old = new Bitmap(canvasPanel.Height,canvasPanel.Width);
+            
+           
+        }
+        private void colorPanel_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void canvasPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawning = true;
+            pStart = new Point(e.X, e.Y);
+            //canvasPanel.Invalidate();
+        }
+
+        private void canvasPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            pEnd = new Point(e.X, e.Y);
+            if (penUse)
+            {
+                myPen = new Pen(colorPanel.BackColor, (int)nudTRackBar.Value); 
+                myGraphics.DrawLine(myPen, pStart, pEnd);
+            }
+            //gdy koniec mniejszy od poczatku???
+            if (rectangleUse)
+            {
+                myPen = new Pen(colorPanel.BackColor, (int)nudTRackBar.Value);
+                myGraphics.DrawRectangle(myPen, pStart.X, pStart.Y, (pEnd.X - pStart.X), (pEnd.Y - pStart.Y));
+            }
+            if (elipseUse)
+            {
+                myPen = new Pen(colorPanel.BackColor, (int)nudTRackBar.Value);
+                myGraphics.DrawEllipse(myPen, pStart.X, pStart.Y, (pEnd.X - pStart.X), (pEnd.Y - pStart.Y));
+            }
+            if (filElipseUse)
+            {
+                myBrush = new SolidBrush(colorPanel.BackColor);
+                myGraphics.FillEllipse(myBrush, pStart.X, pStart.Y, (pEnd.X - pStart.X), (pEnd.Y - pStart.Y));
+            }
+            if (filRectangle)
+            {
+                 myBrush = new SolidBrush(colorPanel.BackColor);
+                myGraphics.FillRectangle( myBrush, pStart.X, pStart.Y, (pEnd.X - pStart.X), (pEnd.Y - pStart.Y));
+            }
+            isDrawning = false;
+        }
+
+        private void canvasPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawning == true)
+            {
+                if (brushUse)
+                {
+                    myGraphics.FillEllipse(myBrush, e.X, e.Y, (int)nudTRackBar.Value, (int)nudTRackBar.Value);
+                }
+                if (eraseUse)
+                {
+                    myBrush = new SolidBrush(Color.White);
+                    myGraphics.FillEllipse(myBrush, e.X, e.Y, (int)nudTRackBar.Value, (int)nudTRackBar.Value);
+                }
+            }
+        }
+        private void colorPanel_DoubleClick(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colorPanel.BackColor = colorDialog1.Color;
+                myBrush.Color = colorPanel.BackColor;
+                myPen.Color = colorPanel.BackColor;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rectangleUse = true;
+            penUse = false;
+            brushUse = false;
+            elipseUse = false;
+            filElipseUse = false;
+            eraseUse = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            penUse = true;
+            rectangleUse = false;
+            brushUse = false;
+            elipseUse = false;
+            filElipseUse = false;
+            eraseUse = false;
+            filRectangle = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            brushUse = true;
+            penUse = false;
+            rectangleUse = false;
+            elipseUse = false;
+            filElipseUse = false;
+            eraseUse = false;
+            filRectangle = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            filElipseUse = false;
+            rectangleUse = false;
+            brushUse = false;
+            penUse = false;
+            elipseUse = true;
+            eraseUse = false;
+            filRectangle = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            nudTRackBar.Text = trackBar1.Value.ToString();
+        }
+
+        private void nudTRackBar_ValueChanged(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)nudTRackBar.Value;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            eraseUse = false;
+            rectangleUse = false;
+            brushUse = false;
+            penUse = false;
+            elipseUse = false;
+            filElipseUse = true;
+            filRectangle = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            rectangleUse = false;
+            brushUse = false;
+            penUse = false;
+            elipseUse = false;
+            filElipseUse = false;
+            eraseUse = true;
+            filRectangle = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            filRectangle = true;
+            rectangleUse = false;
+            brushUse = false;
+            penUse = false;
+            elipseUse = false;
+            filElipseUse = false;
+            eraseUse = false;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            canvasPanel.BackColor = Color.White;
+            myGraphics.Clear(Color.White);
+        }
+        
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            Image obrazek;
+           
+           o.Filter = "bitmapy |*.bmp";
+           o.FilterIndex = 1;
+            if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                obrazek = (Image)Image.FromFile(o.FileName).Clone();
+                obrazek2 = new Bitmap(obrazek, canvasPanel.Width, canvasPanel.Height);
+                canvasPanel.BackgroundImage = obrazek2;
+                myGraphics.DrawImage(obrazek2, startXY);               
+            }
+        }
+
+        private void canvasPanel_SizeChanged(object sender, EventArgs e)
+        {
+
+            
+            
+        }
+        
+
+        private void RotateImage(Panel pb, Image img, float angle)
+        {
+            if (img == null || pb.BackgroundImage == null)
+                return;
+
+            Image oldImage = pb.BackgroundImage;
+            pb.BackgroundImage = Utilities.RotateImage(img, angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+        }
+
+        private void canvasPanel_Resize(object sender, EventArgs e)
+        {
+          
+                Bitmap newBuffer = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+                canvasPanel.DrawToBitmap(newBuffer, new Rectangle(0, 0, canvasPanel.Width, canvasPanel.Height));
+               // if (buffer != null)
+                myGraphics = Graphics.FromImage(newBuffer);
+                myGraphics.DrawImageUnscaled(newBuffer, startXY);
+                myGraphics = canvasPanel.CreateGraphics();
+                
+        }
+        /// <summary>
+        /// cofnij
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Graphics gr1 = canvasPanel.CreateGraphics();
+            Bitmap bmp1 = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+            canvasPanel.DrawToBitmap(bmp1, new Rectangle(0, 0, canvasPanel.Width, canvasPanel.Height));
+            bmp1.Save("d:\\test.jpg");
+
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(canvasPanel.Width, canvasPanel.Height);
+            canvasPanel.DrawToBitmap(bitmap, new System.Drawing.Rectangle(new Point(0, 0), canvasPanel.Size));
+            myGraphics.DrawImage(bitmap, startXY);
+            bitmap.Save("d:\\test2.jpg");
+            gr1.Dispose();
+            myGraphics = canvasPanel.CreateGraphics();
+            
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            initializeTools();
+            Point newPoint = new Point(canvasPanel.Height, 0);
+            Bitmap newBuffer = new Bitmap(canvasPanel.Width, canvasPanel.Height);
+            angle = (float)nudAngle.Value;
+            RotateImage(canvasPanel, newBuffer, angle);
+            myGraphics = canvasPanel.CreateGraphics();
+        }
+        
+    }
+}
